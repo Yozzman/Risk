bb308aa1211a3cd7403ca1fcb0e44987f8741d0f
Yozzman <julienslt@yahoo.fr>
2025-06-20 17:11:08 -1000

    Delete Cell

--- src/pf/yozzman/risk/Deplacement.java
+++ src/pf/yozzman/risk/Deplacement.java
@@ -3,6 +3,4 @@ package pf.yozzman.risk;
 public interface Deplacement {
 	void moveTo(String direction);
 	String getDirection();
-	boolean isOnPosition(Cell c); 
-	void setPosition(Cell[] positions);
 }
