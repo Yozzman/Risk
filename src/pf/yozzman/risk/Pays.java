02dbe663f0ff56f8e3015f8f1b41bd389fc2efa6
Yozzman <julienslt@yahoo.fr>
2025-06-24 17:54:12 -1000

    Initialiser pays

--- src/pf/yozzman/risk/Pays.java
+++ src/pf/yozzman/risk/Pays.java
@@ -1,5 +1,8 @@
 package pf.yozzman.risk;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pays {
 	private String nom;
 	private String voisinNord;
@@ -7,11 +10,19 @@ public class Pays {
     private String voisinEst;
     private String voisinOuest;
     private int nombreTroupe = 0;
+    public String proprietaire;
+    private String continent;
 
-	public Pays (String nom) {
+	public Pays (String nom, String proprietaire, String continent) {
 	    this.nom = nom;
+        this.proprietaire = null;
+        this.continent = continent;
 	}
 	
+    public String getNomContinent() {
+        return this.continent;
+    }
+
 	public String getNom() {
 		return this.nom;
 	}
@@ -31,5 +42,13 @@ public class Pays {
     
     public boolean possedeTroupe() {
     	return nombreTroupe <= 0;
+    }
+
+    private List<Pays> ListePays = new ArrayList<>();
+	    public void initialiserPays() {
+	    ListePays.add(new Pays("Alaska", null, "Amérique du Nord"));
+        ListePays.add(new Pays("Brésil", null, "Amérique du Sud"));
+        ListePays.add(new Pays("Egypte", null, "Afrique"));
+        ListePays.add(new Pays("Inde", null, "Asie"));
     }
 }
