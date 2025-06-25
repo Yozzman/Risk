0b759a983fbbd34a0725b6fcc4ed3da15c322db6
Hugues FIRIAPU <hugues.firiapu@onati.pf>
2025-06-24 16:39:14 -1000

    Fix ConsoleReader and Jeu

--- src/pf/yozzman/risk/ConsoleReader.java
+++ src/pf/yozzman/risk/ConsoleReader.java
@@ -8,26 +8,41 @@ public class ConsoleReader {
 	
 	public Integer demanderNombreJoueur() {
 		Integer nombreFinal = null;
-		while (nombreFinal == null) {
-			
-			System.out.print("Entrer le nombre de joueur : ");
-			Integer nombreEntrer = scanner.nextInt();
 
-			if (nombreEntrer != null) {
-				nombreFinal = nombreEntrer;
-			} 
+		while (nombreFinal == null) {
+
+            System.out.print("Entrez le nombre de joueurs (entre 2 et 5) : ");
+
+			try {
+
+                int entree = Integer.parseInt(scanner.nextLine().trim());
+
+                if (entree >= 2 && entree <= 5) {
+                    nombreFinal = entree;
+                } else {
+					if ( entree < 2) {
+                    System.out.println("Il doit y avoir au moins 2 joueurs.");
+					};
+					if ( entree > 5) {
+                    System.out.println("Il doit y avoir au maximum 5 joueurs.");
+					};
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("Entrée invalide. Veuillez entrer un nombre entier.");
+            }
 		}
 		return nombreFinal;
 	}
 	
-	public  String demanderNomJoueur(Joueur joueur) {
+	public String demanderNomJoueur(Joueur joueur) {
 		String nomFinal = null;
 		while (nomFinal == null) {
 					
 			System.out.print("Nom du joueur " + joueur.getId() + " : ");
 			String nomEntrer = scanner.next();
 			
-			if (nomEntrer == null || nomEntrer.isBlank()) {
+			if (nomEntrer == null || nomEntrer.trim().isEmpty()) {
 				System.out.print("Veuillez réessayer");	
 			}
 			
@@ -37,5 +52,13 @@ public class ConsoleReader {
 		}
 		
 		return nomFinal;
+	}
+
+	public void ajouterJoueur(int nombreJoueur, ListeJoueur<Joueur> listeJoueur) {
+		for (int i = 1; i <= nombreJoueur; i++) {
+        Joueur joueurCreer = new Joueur();
+        String nom = demanderNomJoueur(joueurCreer);
+        joueurCreer.setNom(nom);
+        listeJoueur.ajouterJoueur(joueurCreer);
 	}
 }
--- src/pf/yozzman/risk/Jeu.java
+++ src/pf/yozzman/risk/Jeu.java
@@ -7,18 +7,19 @@ public class Jeu {
 	
 	static ConsoleReader demandeUtilisateur = new ConsoleReader();
 
-	@SuppressWarnings("rawtypes")
-	static ListeJoueur ListeJoueurs = new ListeJoueur();
+	static ListeJoueur<Joueur> listeJoueur = new ListeJoueur<>();
 	
 	static List<Pays> carte = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		
 		int nombreJoueur = demandeUtilisateur.demanderNombreJoueur();
+
+
 		for(int i = 1; i <= nombreJoueur; i++) {
-			Joueur JoueurCreer = new Joueur();
-			JoueurCreer.setNom(demandeUtilisateur.demanderNomJoueur(JoueurCreer));
-			ListeJoueurs.ajouterJoueur(JoueurCreer);
+			Joueur joueurCreer = new Joueur();
+			joueurCreer.setNom(demandeUtilisateur.demanderNomJoueur(joueurCreer));
+			listeJoueur.ajouterJoueur(joueurCreer);
 		}		
 	}
 
